8056f7f83484096b181ccf2d9a6d262cd4cd3544
Philip Cunningham <hello@filib.io>
2018-01-09 16:26:44 +0000

    add linux config.

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -4,7 +4,7 @@ export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 # path to oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="kphoen"
-plugins=(brew gem git git-extras git-flow tmuxinator)
+plugins=(git git-extras git-flow)
 
 # oh-my-zsh settings.
 source $ZSH/oh-my-zsh.sh
@@ -17,13 +17,5 @@ export SHELL="/usr/local/bin/zsh"
 # set default editor to emacs client or fall back to nano.
 export EDITOR="emacsclient"
 
-# checks to see if emacs daemon is currently running.
-function emacs_daemon_running() {
-    ps aux | grep -i "[e]macs --daemon" > /dev/null
-}
-
-# if emacs daemon isn't running, start it up!
-if ! emacs_daemon_running; then
-    echo "starting emacs daemon..."
-    emacs --daemon > /dev/null 2>&1;
-fi
+# gpg
+export GPG_TTY=$(tty)
